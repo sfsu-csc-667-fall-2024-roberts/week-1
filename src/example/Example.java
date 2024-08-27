52b5925df207fca64c32939c7830a7a5896e364b
John Roberts <roberts.john@gmail.com>
2024-08-26 17:57:40 -0700

    Initial commit

---
+++ src/example/Example.java
@@ -0,0 +1,14 @@
+package example;
+public class Example {
+    private int firstNumber;
+    private int secondNumber;
+
+    public Example(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public int add() {
+        return this.firstNumber + this.secondNumber;
+    }
+}
---
+++ tests/ExampleTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import example.Example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    
+    @Test
+    public void testAdd() {
+        Example example = new Example(2, 3);
+
+        assertEquals(5, example.add());
+    }
+}
